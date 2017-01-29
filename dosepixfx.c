0ebc03f87ed9d94da1a2514358653d03076147a7
sean1423 <seanalsop1423@gmail.com>
2017-01-29 17:54:37 +0000

    Add files via upload

--- dosepixfx.c
+++ dosepixfx.c
@@ -203,66 +203,6 @@ CyFxSpiEraseSector (
 }
 
 
-//////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-/* This function initializes the debug module. The debug prints
- * are routed to the UART and can be seen using a UART console
- * running at 115200 baud rate. */
-void
-CyFxApplnDebugInit (void)
-{
-    CyU3PUartConfig_t uartConfig;
-    CyU3PReturnStatus_t apiRetStatus = CY_U3P_SUCCESS;
-
-    /* Initialize the UART for printing debug messages */
-    apiRetStatus = CyU3PUartInit();
-    if (apiRetStatus != CY_U3P_SUCCESS)
-    {
-        /* Error handling */
-        CyFxAppErrorHandler(apiRetStatus);
-    }
-
-    /* Set UART configuration */
-    CyU3PMemSet ((uint8_t *)&uartConfig, 0, sizeof (uartConfig));
-    uartConfig.baudRate = CY_U3P_UART_BAUDRATE_115200;
-    uartConfig.stopBit = CY_U3P_UART_ONE_STOP_BIT;
-    uartConfig.parity = CY_U3P_UART_NO_PARITY;
-    uartConfig.txEnable = CyTrue;
-    uartConfig.rxEnable = CyFalse;
-    uartConfig.flowCtrl = CyFalse;
-    uartConfig.isDma = CyTrue;
-
-    apiRetStatus = CyU3PUartSetConfig (&uartConfig, NULL);
-    if (apiRetStatus != CY_U3P_SUCCESS)
-    {
-        CyFxAppErrorHandler(apiRetStatus);
-    }
-
-    /* Set the UART transfer to a really large value. */
-    apiRetStatus = CyU3PUartTxSetBlockXfer (0xFFFFFFFF);
-    if (apiRetStatus != CY_U3P_SUCCESS)
-    {
-        CyFxAppErrorHandler(apiRetStatus);
-    }
-
-    /* Initialize the debug module. */
-    apiRetStatus = CyU3PDebugInit (CY_U3P_LPP_SOCKET_UART_CONS, 8);
-    if (apiRetStatus != CY_U3P_SUCCESS)
-    {
-        CyFxAppErrorHandler(apiRetStatus);
-    }
-
-    CyU3PDebugPreamble(CyFalse);
-}
-
 
 void
 USBTOGPIFDmaCallback (
@@ -405,12 +345,6 @@ CyFxApplnStart (
 
 
     //Defining Debug Endpoint
-    apiRetStatus = CyU3PDebugInit (CY_FX_EP_DEBUG_SOCKET, 8); // This line is needed for usb debugger. Will need different socket.
-        if (apiRetStatus != CY_U3P_SUCCESS)
-        {
-            CyFxAppErrorHandler (apiRetStatus);
-        }
-
         CyU3PMemSet ((uint8_t *)&epCfg, 0, sizeof (epCfg));
             epCfg.enable = CyTrue;
             epCfg.epType = CY_U3P_USB_EP_BULK;
@@ -418,7 +352,7 @@ CyFxApplnStart (
             epCfg.streams = 0;
             epCfg.pcktSize = size;
 
-            /* USB Producer endpoint configuration */
+            /* USB debugger endpoint configuration */
             apiRetStatus = CyU3PSetEpConfig (CY_FX_EP_DEBUG_SOCKET, &epCfg);
             if (apiRetStatus != CY_U3P_SUCCESS)
             {
@@ -553,10 +487,6 @@ CyFxApplnStart (
 
 
 
-
-
-
-    ////////////////////////////////////////////////////////////////////
 #ifdef STREAMING_MANUAL
     dmaCfg.notification = CY_U3P_DMA_CB_CONS_SUSP | CY_U3P_DMA_CB_PROD_EVENT;
     dmaCfg.cb = GPIFTOUSBDmaCallback;
@@ -579,23 +509,6 @@ CyFxApplnStart (
         CyU3PDebugPrint (4, "CyU3PDmaChannelSetXfer failed, Error code = %d\n", apiRetStatus);
         CyFxAppErrorHandler(apiRetStatus);
     }
-
-
-    ////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
